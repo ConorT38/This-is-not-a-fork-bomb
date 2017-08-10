88fd69ef5c484a346e2d2b10b660c821374486ba
Conor Thompson <conorthompson213@gmail.com>
2017-08-10 14:56:31 +0100

    Add files via upload

---
+++ Script.bat
@@ -0,0 +1,12 @@
+  :: Open a Telnet window
+   start telnet.exe 10.201.37.141 11211
+   :: Run the script 
+   cscript SendKeys.vbs 
+
+   start telnet.exe 10.201.37.142 11211
+   cscript SendKeys.vbs 
+
+   start telnet.exe 10.201.37.143 11211
+   cscript SendKeys.vbs 
+
+   ipconfig /flushdns
